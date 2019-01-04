5216894f3e31c30c328c25a1d1291266233ed014
Sara Varisara <varisara.n@gmail.com>
2019-01-04 04:32:00 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+
+  def initialize(name)
+    @name = name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+end
+
+beyonce = Person.new("Beyonce")
+beyonce.name
